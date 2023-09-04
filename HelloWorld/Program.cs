a705cbe068e133e327118dc290c4231573731ddc
Gustavo <gustgouvea@hotmail.com>
2023-09-04 10:32:10 -0300

    c#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Int - -156 como 32334     Números inteiros
+            // Flot - 15.6 como 232.132  Números com casas decimais
+            // Bool - true ou false
+            // String - "Texto"
+            // Char - 'a'   Apenas um caracter
+
+            // A-Z
+            // 0 - 9
+            // @ $ .
+            // _
+
+            int segundaGuerraMundial = 1942;
+            string corFavorita = "Vermelho";
+            float velocidadeF1 = 294.48f;
+            bool segundaGuerraMundialAconteceu = true;
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Meu nome é Gustavo Gouvêa");
+
+            Console.ReadLine();
+
+        }
+    }
+}
